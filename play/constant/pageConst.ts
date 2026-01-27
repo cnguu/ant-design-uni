5b55716b0336efe9edd43ea4aae8f0b3a263c4eb
cnguu <www@cnguu.cn>
2026-01-27 14:04:11 +0800

    Update scripts and dependencies, remove cross-env, and downgrade ora
    - Update `play` package scripts to use `UNI_INPUT_DIR` without `cross-env`
    - Remove `cross-env` from `play` and root `package.json`
    - Downgrade `ora` to version 8.2.0
    - Update related dependencies and configurations in `pnpm-lock.yaml`

--- play/constant/pageConst.ts
+++ play/constant/pageConst.ts
@@ -1,12 +1,15 @@
-/*******************************
+
+    /*******************************
      * 此文件由脚本自动生成，请勿手动修改 *
      *******************************/
 
-/** 页面路径常量 */
-export const PageUrlConst = {
-  /** 首页 */
-  PAGE_HOME_INDEX_PAGE: '/page/home/index-page',
+    /** 页面路径常量 */
+    export const PageUrlConst = {
+/** 首页 */
+PAGE_HOME_INDEX_PAGE: '/page/home/index-page',
 
-  /** 分包 */
-  PAGE_A_INDEX_PAGE: '/page-a/index-page',
+/** 分包 */
+PAGE_A_INDEX_PAGE: '/page-a/index-page',
+
 } as const
+    
